83bdad4396679e911b91b119a0b0a16b01705e64
Shihab Islam <56773545+ShihabIslam789@users.noreply.github.com>
2023-11-29 19:17:58 -0500

    fix cookie msg by not showing password

--- api/index.js
+++ api/index.js
@@ -1,20 +1,83 @@
-const express = require('express')
+const express = require('express');
 const cors = require('cors');
+const mongoose = require("mongoose")
+const User = require('./models/User.js')
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
+
+
+
+require('dotenv').config();
 const app = express();
 
+const bcryptSalt = bcrypt.genSaltSync(10);
+const jwtSecret = 'fasefraw4r5r3wq45wdfgw34twdfg';
+
 app.use(express.json());
+app.use(cookieParser())
 app.use(cors({
     credentials: true,
     origin: 'http://127.0.0.1:5173',
 }));
 
+Mongoose.connect('process.env.MONGO_URL')
+
 app.get('/test',(req,res) => {
     res.json('test ok');
 });
 
-app.post('/register', (req,res) => {
+//
+app.post('/register', async (req,res) => {
     const {name,email,password} = req.body;
-    res.join({name,email,password});
+    try{
+        const userDoc = await User.create({
+            name,
+            email,
+            password:bcrypt.hashSync(password, bcryptSalt),
+        });
+
+        res.json(userDoc)
+    } catch (e) {
+        res.status(442).json(e);
+    }
 });
 
+
+app.post('/Login', async (req,res)=> {
+    const {email,password} = req.body;
+    const userDoc = await User.findOne({email});
+    if (userDoc) {
+        const passOk = bcrypt.compareSync(password, userDoc.password)
+        if (passOk) {
+            jwt.sign({
+                email:userDoc,email,
+                id:userDoc, id, 
+            }, jwtSecret, {}, (err,token) =>{
+                if (err) throw err;
+                res.cookie('token','token')('pass ok');
+
+            });
+        }
+        }else {
+        res.join('pass not ok');
+        } else {
+        res.join('not found');
+    }
+});
+
+app.get('/profile', (req,res) => {
+   const {token} = req.cookies;
+   if (token) {
+        jwt.verify(token, jwtSecret,{}, async (err,user) =>{
+            if(err) throw err;
+            const {name,email,_id} = await User.findById(userdata.id);
+            res.json({name,email,_id});
+        });
+   }else {
+    res.json(null);
+   }
+
+   res.token({token})
+})
 app.listen(4000);
